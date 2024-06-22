ac42db304f69673930f8f38422bae1556bfaacb2
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-22 19:11:10 +0200

    ajout du constructeur db

--- assets/Class/Game.php
+++ assets/Class/Game.php
@@ -1,7 +1,13 @@
 <?php
     require_once('../../Database.php');
 
-    class Game{
+    class Game extends Database {
+        //Définition du constructeur
+        public function __construct() {
+            // Appelle le constructeur de la classe parente pour initialiser la connexion
+            parent::__construct();
+            $this->conn = $this->connect();
+        }
 
     }
 ?>
