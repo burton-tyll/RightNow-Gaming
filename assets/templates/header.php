7220500bea50e54c5b940663e7c2cd12e9f70262
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-24 16:30:54 +0200

    ajout du logo page accueil

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,7 +1,7 @@
 
 <header>
     <nav>
-        <img src="../img/logo.png" alt="logo" class="logo">
+        <a href="../../index.php"><img src="../img/logo.png" alt="logo" class="logo"></a>
         <ul>
             <li><a href="/pc"><img src="../img/pc.png" alt="pc_logo"><p>PC</p></a></li>
             <li><a href="/playstation"><img src="../img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
