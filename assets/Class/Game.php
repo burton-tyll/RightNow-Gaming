81ddde4ab276eefa7cd1d8d15b0e34f16d0c0450
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-25 00:12:54 +0200

    ajout des fonctions pour récupérer les jeux selon la plateforme

--- assets/Class/Game.php
+++ assets/Class/Game.php
@@ -43,5 +43,34 @@
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
+        //Playstation Games
+
+        public function getAllGamesByPlateform($id_platform){
+            $query = 'SELECT id_game FROM game_platform WHERE id_platform = :id_platform';
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute(['id_platform' => $id_platform]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function getAllGamesByPlatformOrderedByDate($id_platform) {
+            $query = 'SELECT * FROM game JOIN game_platform ON game.id = game_platform.id_game WHERE game_platform.id_platform = :id_platform ORDER BY game.release_date DESC';
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute(['id_platform' => $id_platform]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function getAllGamesByPlatformOrderedByRate($id_platform) {
+            $query = 'SELECT * FROM game JOIN game_platform ON game.id = game_platform.id_game WHERE game_platform.id_platform = :id_platform ORDER BY game.rate DESC';
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute(['id_platform' => $id_platform]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function getTheBestGameByPlateform($id_platform){
+            $query = 'SELECT * FROM game JOIN game_platform ON game.id = game_platform.id_game WHERE game_platform.id_platform = :id_platform ORDER BY game.rate DESC LIMIT 1';
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute(['id_platform' => $id_platform]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 ?>
