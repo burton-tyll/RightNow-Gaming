590ece224e4151ce24cf415ed5a5cb34d9ccecea
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-24 14:33:05 +0200

    suppresion header de la branche dev

--- assets/script/global.js
+++ assets/script/global.js
@@ -27,3 +27,7 @@ document.getElementById('loadImage').addEventListener('click', () => {
     const img = document.getElementById('randomImage');
     img.src = `https://picsum.photos/200/300?random=${new Date().getTime()}`;
 });
+
+
+
+
