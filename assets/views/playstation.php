f05035a979ed0efa4c021cbacf71a290a87ac279
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-24 21:22:51 +0200

    ajout des liens du header

--- assets/views/playstation.php
+++ assets/views/playstation.php
@@ -34,10 +34,10 @@
         <nav>
             <img src="./assets/img/logo.png" alt="logo" class="logo">
             <ul>
-                <li><a href="./assets/views/pc.php"><img src="./assets/img/pc.png" alt="pc_logo"><p>PC</p></a></li>
-                <li><a href="./assets/views/playstation.php"><img src="./assets/img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
-                <li><a href="./assets/views/xbox.php"><img src="./assets/img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
-                <li><a href="./assets/views/nintendo.php"><img src="./assets/img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
+                <li><a href="?pc"><img src="./assets/img/pc.png" alt="pc_logo"><p>PC</p></a></li>
+                <li><a href="?playstation"><img src="./assets/img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
+                <li><a href="?xbox"><img src="./assets/img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
+                <li><a href="?nintendo"><img src="./assets/img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
             </ul>
         </nav>
     </header>
