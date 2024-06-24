43b51dd4b1c743758d21117d46b07c29da3d2a83
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-24 10:14:10 +0200

    ajout du header

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,10 +1,22 @@
-<header>
-    <nav>
-        <ul>
-            <li><a href=""><img src="" alt=""><p>PC</p></a></li>
-            <li><a href=""><img src="" alt=""><p>Playstation</p></a></li>
-            <li><a href=""><img src="" alt=""><p>Xbox</p></a></li>
-            <li><a href=""><img src="" alt=""><p>Nintendo</p></a></li>
-        </ul>
-    </nav>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/global.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href=""><img src="" alt=""><p>PC</p></a></li>
+                <li><a href=""><img src="" alt=""><p>Playstation</p></a></li>
+                <li><a href=""><img src="" alt=""><p>Xbox</p></a></li>
+                <li><a href=""><img src="" alt=""><p>Nintendo</p></a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
+
