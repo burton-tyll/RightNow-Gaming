131f016341d1d68bf800f0195a7c16a41717ae4e
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-24 11:06:38 +0200

    ajout du header v1

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/global.css">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href=""><img src="" alt=""><p>PC</p></a></li>
-                <li><a href=""><img src="" alt=""><p>Playstation</p></a></li>
-                <li><a href=""><img src="" alt=""><p>Xbox</p></a></li>
-                <li><a href=""><img src="" alt=""><p>Nintendo</p></a></li>
-            </ul>
-        </nav>
-    </header>
-</body>
-</html>
+
+<header>
+    <nav>
+        <img src="../img/logo.png" alt="logo" class="logo">
+        <ul>
+            <li><a href="/pc"><img src="../img/pc.png" alt="pc_logo"><p>PC</p></a></li>
+            <li><a href="/playstation"><img src="../img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
+            <li><a href="/xbox"><img src="../img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
+            <li><a href="/nintendo"><img src="../img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
+        </ul>
+    </nav>
+</header>
+
 
--- index.php
+++ index.php
@@ -8,6 +8,16 @@
     <title>RightNow Gaming</title>
 </head>
 <body>
-    <?php include("./assets/templates/header.php") ?>
+    <header>
+        <nav>
+            <img src="./assets/img/logo.png" alt="logo" class="logo">
+            <ul>
+                <li><a href="/pc"><img src="./assets/img/pc.png" alt="pc_logo"><p>PC</p></a></li>
+                <li><a href="/playstation"><img src="./assets/img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
+                <li><a href="/xbox"><img src="./assets/img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
+                <li><a href="/nintendo"><img src="./assets/img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
