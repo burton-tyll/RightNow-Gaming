af23f56e9f1c556a8b59c375421bc6276695bb9e
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-25 14:04:55 +0200

    ajout du dropdown profil avec le bouton de connexion/deconnexion

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,3 +1,26 @@
+<?php
+
+    function getStatus() {
+        if (!isset($_SESSION['user'])) {
+            return 'Connexion';
+        } else {
+            return 'Déconnexion';
+        }
+    }
+    
+    function disconnect() {
+        session_destroy();
+        header("Location: connexion.php"); // Redirection après déconnexion
+        exit();
+    }
+    
+    if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+        disconnect();
+    } 
+    $status = getStatus();
+
+?>
+
 
 <header>
     <nav>
@@ -7,6 +30,16 @@
             <li><a href="./playstation.php"><img src="../img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
             <li><a href="./xbox.php"><img src="../img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
             <li><a href="./nintendo.php"><img src="../img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
+        </ul>
+        <ul class="profil-dropdown">
+            <li>Profil</li>
+            <li>
+                <?php if ($status == 'Connexion'): ?>
+                    <a href="connexion.php"><?php echo $status; ?></a>
+                <?php else: ?>
+                    <a href="?action=logout"><?php echo $status; ?></a>
+                <?php endif; ?>
+            </li>
         </ul>
     </nav>
 </header>
--- assets/views/connexion.php
+++ assets/views/connexion.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             if($validUser){
                 session_start();
                 $_SESSION['user'] = $username;
-                header('Location: ./homepage.php');
+                header('Location: ../../index.php');
                 exit;
             } 
         }
