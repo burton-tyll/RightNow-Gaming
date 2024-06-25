127eb264c646114831e4b0f5d32d56b50470c42d
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-25 15:04:26 +0200

    ajout du bouton dropdown sur la page d'accueil

--- index.php
+++ index.php
@@ -61,16 +61,19 @@
                 <li><a href="./assets/views/xbox.php"><img src="./assets/img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
                 <li><a href="./assets/views/nintendo.php"><img src="./assets/img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
             </ul>
+            <div class="user-buttons">
+                <img src="./assets/img/user.png" alt="userImage" class="userButton">
+            </div>
             <ul class="profil-dropdown">
-            <li><a href="">Profil</a></li>
-            <li><a href="">Mes achats</a></li>
-            <li>
-                <?php if ($status == 'Connexion'): ?>
-                    <a href="./assets/views/connexion.php"><?php echo $status; ?></a>
-                <?php else: ?>
-                    <a href="?action=logout"><?php echo $status; ?></a>
-                <?php endif; ?>
-            </li>
+                <li><a href="">Profil</a></li>
+                <li><a href="">Mes achats</a></li>
+                <li>
+                    <?php if ($status == 'Connexion'): ?>
+                        <a href="./assets/views/connexion.php"><?php echo $status; ?></a>
+                    <?php else: ?>
+                        <a href="?action=logout"><?php echo $status; ?></a>
+                    <?php endif; ?>
+                </li>
         </ul>
         </nav>
     </header>
