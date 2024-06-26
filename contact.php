bbd5846e3e7339a25c12afebe7350082bbad87bb
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-06-26 14:40:14 +0200

    changement mdp db pour mac

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    echo "Votre message a été envoyé avec succès.";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <?php include('./assets/templates/global.php') ?>
+    <title>Formulaire de contact</title>
+</head>
+<body>
+    <h1>Nous contacter</h1>
+    <form action="contact.php" method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="objet">Objet:</label>
+        <input type="text" id="objet" name="objet" required>
+        <br>
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+        <br>
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+</html>
