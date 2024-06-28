035188be361cd8f3eae54e92f66ec2491b0c6c77
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-06-28 09:24:28 +0200

    ajout du style et du header à la page contact

--- assets/views/contact.php
+++ assets/views/contact.php
@@ -2,11 +2,27 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
+    $objet = $_POST['objet'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    echo "Votre message a été envoyé avec succès.";
+    $to = 'aaliyah.houidef@laplateforme.io'; 
+    $subject = 'Nouveau message de contact: ' . $objet;
+    $body = "Vous avez reçu un nouveau message de contact.\n\n".
+            "Email: $email\n".
+            "Objet: $objet\n".
+            "Message:\n$message";
+
+            $headers = "From: $email\r\n";
+            $headers .= "Reply-To: $email\r\n";
+            $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+        
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Votre message a été envoyé avec succès.";
+    } else {
+        echo "Une erreur s'est produite lors de l'envoi de votre message.";
+    }
 }
 ?>
 
@@ -26,22 +42,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Formulaire de contact</title>
 </head>
 <body>
+    <!--Navbar-->
     <?php include('../templates/header.php') ?>
-    <!--Accueil-->
+    <!---------->
     <main>
         <h1>Nous contacter</h1>
-        <form action="contact.php" method="post">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br>
-            <label for="objet">Objet:</label>
-            <input type="text" id="objet" name="objet" required>
-            <br>
-            <label for="message">Message:</label>
-            <textarea id="message" name="message" required></textarea>
-            <br>
-            <button type="submit">Envoyer</button>
+        <div class="form-container">
+        <form action="contact.php" method="post" class="contact-form">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <label for="objet">Objet:</label>
+                <input type="text" id="objet" name="objet" required>
+                <label for="message">Votre message:</label>
+                <textarea id="message" name="message" required></textarea>
+                <button type="submit">Envoyer</button>
         </form>
+        </div>
     </main>
 </body>
 </html>
