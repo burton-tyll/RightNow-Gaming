0e380d4ec7f8e70aeadaab895ab7b4fc61f78cbe
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-06-28 16:22:55 +0200

    ajout des liens sur la page pc

--- assets/templates/global.php
+++ assets/templates/global.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" href="../styles/global.css">
 <link rel="icon" type="image/x-icon" href="../img/favicon.png">
 <script src="../script/global.js" defer></script>
+<script src="../script/promo.js" defer></script>
