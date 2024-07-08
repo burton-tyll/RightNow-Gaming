c65f33ce5636671d6ce8657aa7b59e925b756820
Younes Kribeche <younes.kribeche@laplateforme.io>
2024-07-08 10:17:10 +0200

    finitions page cart.php

--- assets/views/cart.php
+++ assets/views/cart.php
@@ -10,6 +10,9 @@ $game_genres = new Genre();
 session_start();
 
 $cartItems = [];
+$totalPrice = 0; // Prix total sans réduction
+$totalDiscountedPrice = 0; // Prix total avec réduction
+$totalSavings = 0; // Économies totales
 
 if (isset($_SESSION['cart'])) {
     foreach ($_SESSION['cart'] as $gameId => $data) {
@@ -18,14 +21,31 @@ if (isset($_SESSION['cart'])) {
             $gamePlatforms = $game_platforms->getGamePlateform($gameId);
             foreach ($data['platforms'] as $platformId => $quantity) {
                 $platformName = isset($gamePlatforms[$platformId]) ? $gamePlatforms[$platformId] : 'Plateforme inconnue';
+                
+                // Calcul du prix avec la réduction en pourcentage
+                $price = $gameDetails['price'];
+                $specialOffer = $gameDetails['special_offer'];
+                $discountedPrice = $price;
+                if ($specialOffer && $specialOffer > 0) {
+                    // Calculer le prix après réduction
+                    $discountedPrice = $price * (1 - ($specialOffer / 100));
+                }
+
                 $cartItems[] = [
                     'id' => $gameId,
                     'name' => $gameDetails['name'],
-                    'price' => $gameDetails['price'],
+                    'price' => $price,
+                    'discountedPrice' => $discountedPrice,
                     'image' => $gameDetails['image'],
                     'platform' => $platformName,
-                    'quantity' => $quantity
+                    'quantity' => $quantity,
+                    'special_offer' => $specialOffer
                 ];
+
+                // Calculer les montants totaux
+                $totalPrice += $price * $quantity;
+                $totalDiscountedPrice += $discountedPrice * $quantity;
+                $totalSavings += ($price - $discountedPrice) * $quantity;
             }
         }
     }
@@ -68,39 +88,62 @@ function convertBlobToBase64($blob) {
     </header>
     <main>
         <section id="cart-section">
-            <h1>Mon Panier</h1>
-            
-            <!-- Vérifier si le panier est vide -->
-            <?php if (empty($cartItems)) :?>
-                <h1>Le panier est vide!</h1>
-                <a href="../../index.php">Retourner aux achats</a>
-            <?php else: ?>
-                <ul id="cart-items-list">
-                    <?php foreach ($cartItems as $item): ?>
-                        <li class="cart-item">
-                            <div class="item-details">
-                                <h2 class="item-name"><?php echo htmlspecialchars($item['name']); ?></h2>
-                                <p class="item-price"><?php echo htmlspecialchars($item['price']) . ' €'; ?></p>
-                                <p class="item-quantity">Quantité: <?php echo htmlspecialchars($item['quantity']); ?></p>
-                                <p class="item-platform">Plateforme: <?php echo htmlspecialchars($item['platform']); ?></p>
-                            </div>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-
-                <div id="cart-summary">
-                    <?php
-                    $totalPrice = array_reduce($cartItems, function ($sum, $item) {
-                        return $sum + ($item['price'] * $item['quantity']);
-                    }, 0);
-                    ?>
-                    <p id="total-price">Prix total: <?php echo number_format($totalPrice, 2, ',', ' ') . ' €'; ?></p>
+            <div>
+                <h2>Mon Panier:</h2>
+                <!-- Vérifier si le panier est vide -->
+                <?php if (empty($cartItems)) :?>
+                    <h1>Le panier est vide!</h1>
+                    <a href="../../index.php">Retourner aux achats</a>
+                <?php else: ?>
+                    <ul id="cart-items-list">
+                        <?php foreach ($cartItems as $item): ?>
+                            <li class="cart-item">
+                                <div class="item-details">
+                                    <div>
+                                        <img src="<?php echo convertBlobToBase64($item['image']); ?>" alt="Image du jeu" class="item-image">
+                                    </div>
+                                    <div class="item-name-price-platform-name-quantity">
+                                        <p class="item-name"><strong><?php echo htmlspecialchars($item['name']); ?></strong></p>
+                                        <div id="div-special-offer-price">
+                                            <?php if ($item['special_offer'] && $item['special_offer'] > 0): ?>
+                                                <div>
+                                                <p id="price-before-special-offer"><?php echo number_format($item['price'], 2, ',', ' ') . ' €'; ?></p>
+                                                <p id="special-offer"><?php echo '-' . htmlspecialchars($item['special_offer']) . '%'; ?></p>
+                                                </div>
+                                            <?php endif; ?>
+                                            <p id="discounted-price"><?php echo number_format($item['discountedPrice'], 2, ',', ' ') . ' €'; ?></p>
+                                        </div>
+                                        <p class="item-platform-name"><strong><?php echo htmlspecialchars($item['platform']); ?></strong></p>
+                                        <p class="item-quantity">Quantité: <?php echo htmlspecialchars($item['quantity']); ?></p>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
-
-                <a href="../../index.php" id="continue-shopping">Continuer vos achats</a>
-                <a href="checkout.php" id="checkout">Passer à la caisse</a>
+                <div id="cart-summary">
+                    <h2>Résumé:</h2>
+                    <div id="cart-summary-prices">
+                        <div id="price-ammount">
+                        <div id="price-titles">
+                        <p class="total-price">Prix de base: </p>
+                        <p class="total-savings">Économies totales:</p>
+                        <p id="discounted-price-final">Total: </p>
+                        </div>
+                        <div id="prices">
+                        <p class="total-price"><?php echo number_format($totalPrice, 2, ',', ' ') . ' €'; ?></p>
+                        <p class="total-savings"><?php echo number_format($totalSavings, 2, ',', ' ') . ' €'; ?></p>
+                        <p><?php echo number_format($totalDiscountedPrice, 2, ',', ' ') . ' €'; ?></p>
+                        </div>
+                        </div>
+                        <div id="go-back-paiement-links">
+                        <a href="checkout.php" id="checkout">Passer à la caisse</a>
+                        <p>ou</p>
+                        <a href="../../index.php" id="continue-shopping">Continuer vos achats</a>
+                        </div>
+                    </div>
+                </div>
             <?php endif; ?>
-
         </section>
     </main>
 </body>
--- assets/views/game-details.php
+++ assets/views/game-details.php
@@ -12,7 +12,51 @@ $game_platforms = new Game_platform();
 $game_genres = new Genre();
 $comment = new Comment();  
 
+//---------------
+    //---------------GESTIONNAIRE DE CONNEXION UTILSATEUR
+    //---------------
 
+    function getStatus() {
+        if (!isset($_SESSION['user'])) {
+            return 'Connexion';
+        } else {
+            return 'Déconnexion';
+        }
+    }
+    
+    function disconnect() {
+        session_destroy();
+        header("Location: connexion.php"); // Redirection après déconnexion
+        exit();
+    }
+    
+    if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+        disconnect();
+    }
+    $status = getStatus();
+
+    //GESTIONNAIRE D'ADMINISTRATION
+
+    function getRole(){
+        if (isset($_SESSION['admin'])){
+            return 'Admin';
+        }else{
+            return 'User';
+        }
+    }
+
+    $role = getRole();
+
+    // Calculer le nombre total d'articles dans le panier
+    function getCartCount() {
+        if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
+            return 0;
+        }
+    
+        return array_sum(array_column($_SESSION['cart'], 'quantity'));
+    }
+    
+    $totalItems = getCartCount();
 
 // Vérifier que l'utilisateur est connecté
 $userId = isset($_SESSION['user_id']) ? intval($_SESSION['user_id']) : null;
@@ -204,8 +248,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
 </head>
 <body>
-    <!-- Inclusion de l'en-tête -->
-    <?php include('../templates/header.php') ?>
+
+<header>
+    <nav>
+        <a href="../../index.php" class="logo"><img src="../img/logo.png" alt="logo"></a>
+        <ul>
+            <li><a href="./pc.php"><img src="../img/pc.png" alt="pc_logo"><p>PC</p></a></li>
+            <li><a href="./playstation.php"><img src="../img/playstation.png" alt="playstation_logo"><p>Playstation</p></a></li>
+            <li><a href="./xbox.php"><img src="../img/xbox.png" alt="xbox_logo"><p>Xbox</p></a></li>
+            <li><a href="./nintendo.php"><img src="../img/nintendo.png" alt="nintendo_logo"><p>Nintendo</p></a></li>
+        </ul>
+            
+        <div class="user-buttons">
+            <a href="./cart.php" id="icon-cart">
+                <img src="../img/icon-cart.svg" alt="img cart"></img>
+                <p id="total-items"><?php echo $totalItems; ?></p>
+            </a>
+            <img src="../img/user.png" alt="userImage" class="userButton">
+        </div>
+        <ul class="profil-dropdown">
+            <li><a href="">Profil</a></li>
+            <?php if ($role == 'Admin'): ?>
+                <li><a href="paneladmin.php">Panel admin</a></li>
+            <?php endif ?>
+            <li><a href="">Mes achats</a></li>
+            <li>
+                <?php if ($status == 'Connexion'): ?>
+                    <a href="connexion.php"><?php echo $status; ?></a>
+                <?php else: ?>
+                    <a href="?action=logout"><?php echo $status; ?></a>
+                <?php endif; ?>
+            </li>
+        </ul>
+
+    </nav>
+</header>
+
 
     <main>
         <div id="section-game-details">
